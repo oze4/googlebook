9ad58f4166a4cfe436bfe8cc307c04083b211e70
Marc <marcjfj@gmail.com>
2020-02-20 22:52:14 -0600

    initial

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const path = require('path');
+const Axios = require('axios');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3001;
+const mongoUri = process.env.MONGO || 'mongodb://localhost:27017/book';
+
+const app = express();
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+const { Schema } = mongoose;
+const bookSchema = new Schema({
+  info: Schema.Types.Mixed,
+});
+const Book = mongoose.model('Book', bookSchema);
+
+app.post('/api/search', (req, res) => {
+  Axios.get(
+    `https://www.googleapis.com/books/v1/volumes?q=${req.body.term}`
+  ).then(books => res.json(books.data.items));
+});
+app.post('/api/save', (req, res) => {
+  const newBook = new Book({ info: req.body.book });
+  newBook.save(err => {
+    if (err) res.json(err);
+    res.json({ status: true });
+  });
+});
+app.post('/api/unsave', (req, res) => {
+  Book.findByIdAndRemove(req.body.book._id, err => {
+    if (err) res.json(err);
+    res.json({ status: true });
+  });
+});
+app.get('/api/saved', (req, res) => {
+  Book.find({}, (err, books) => {
+    if (err) res.json(err);
+    res.json(books);
+  });
+});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
+
+mongoose.connect(mongoUri, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected');
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
