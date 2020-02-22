00433b2a703495311b5be6266679cd00c731195d
Matt Oestreich <matthewpoestreich@gmail.com>
2020-02-22 16:02:11 -0600

    Fixed Issues

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 const Axios = require('axios');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const MongoClient = require('mongodb').MongoClient
-
-require('dotenv').config();
 
 const PORT = process.env.PORT || 8080;
 const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/book';
@@ -22,9 +21,11 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const { Schema } = mongoose;
+
 const bookSchema = new Schema({
   info: Schema.Types.Mixed,
 });
+
 const Book = mongoose.model('Book', bookSchema);
 
 app.post('/api/search', (req, res) => {
@@ -35,19 +36,11 @@ app.post('/api/search', (req, res) => {
 });
 
 app.post('/api/save', (req, res) => {
-  console.log('got request');
-  res.status(200).end();
-  /*
-  try {
-    const newBook = new Book({ info: req.body.book });
-    newBook.save(err => {
-      if (err) res.json(err);
-      res.json({ status: true });
-    });
-  } catch (err) {
-    res.status(200).send({ error: err });
-  }
-  */
+  const newBook = new Book({ info: req.body.book });
+  newBook.save(err => {
+    if (err) res.json(err);
+    res.json({ status: true });
+  });
 });
 
 app.post('/api/unsave', (req, res) => {
@@ -58,40 +51,45 @@ app.post('/api/unsave', (req, res) => {
 });
 
 app.get('/api/saved', (req, res) => {
-  console.log("in /api/saved");
-  try {
-    Book.find({}, (err, books) => {
-      if (err) res.json(err);
-      res.json(books);
-    });
-  } catch (err) {
-    res.status(200).send({ error: err });
-  }
-});
-
-app.get('/api/test', (req, res) => {
-  res.status(200).send("Got it!");
+  Book.find({}, (err, books) => {
+    if (err) res.json(err);
+    else res.json(books);
+  });
 });
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
-mongoose.connect(mongoUri, { 
-  useNewUrlParser: true ,
-  // useUnifiedTopology: true,  // Added to get rid of deprecation warnings
-  // useFindAndModify: false    // Added to get rid of deprecation warnings
-});
-
+/*
 const db = mongoose.connection;
 
-db.on('error', /* console.error.bind(console, 'connection error:') */ error => {
-  console.log("[MONGOOSE][ERROR]", error);
-});
+db.on('error', // console.error.bind(console, 'connection error:') 
+  error => {
+    console.log("[MONGOOSE][ERROR]", error);
+  }
+);
 
 db.once('open', function() {
   console.log('[MONGOOSE][SUCCESS] Connected to database!');
 });
+*/
+
+const connectDB = async (db) => {
+  try {
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useFindAndModify: false,
+    });
+    console.log("MongoDB is Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+connectDB(mongoUri);
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
