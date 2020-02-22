978a14100b803f98effc9212374e8d7eaee6a00e
Matt Oestreich <matthewpoestreich@gmail.com>
2020-02-22 14:12:59 -0600

    Fixed Issues

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 8080;
-const mongoUri = process.env.MONGODB_URI // || 'mongodb://localhost:27017/book';
+const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/book';
 
 const app = express();
 
